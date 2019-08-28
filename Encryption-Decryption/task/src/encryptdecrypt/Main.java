8149e1a97f93cf965105faad8da2796175490f5a
AhmetY7 <ahmetylmz95@gmail.com>
2019-08-28 22:18:42 +0300

    Stage #3 completed

--- Encryption-Decryption/task/src/encryptdecrypt/Main.java
+++ Encryption-Decryption/task/src/encryptdecrypt/Main.java
@@ -1,21 +1,33 @@
 package encryptdecrypt;
+
 import java.util.Scanner;
+
 public class Main {
+    private static void decryption(String message, int key) {
+        for(int i=0; i<message.length(); i++) {
+            char temp = message.charAt(i);
+            temp -=key;
+            System.out.print(temp);
+        }
+    }
+
+    private static void encryption(String message, int key) {
+        for(int i=0; i<message.length(); i++) {
+            char temp = message.charAt(i);
+            temp +=key;
+            System.out.print(temp);
+        }
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String operation = scanner.nextLine();
         String message = scanner.nextLine();
         int key = scanner.nextInt();
-        for(int i=0; i<message.length();i++) {
-            char temp = message.charAt(i);
-            if(temp>=97 && temp<=122){
-                temp += key;
-                if(temp > 122){
-                    temp = (char)(96 + (temp - 122));
-                }
-                System.out.print(temp);
-            } else if(message.charAt(i) == ' '){
-                System.out.print(" ");
-            }
+        if("dec".equals(operation)) {
+            decryption(message,key);
+        } else {
+            encryption(message,key);
         }
     }
 }
