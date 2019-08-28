060681c9b742596fa7c2f8a24928bc3e690e5920
AhmetY7 <ahmetylmz95@gmail.com>
2019-08-28 21:05:03 +0300

    Stage #2 completed

--- Encryption-Decryption/task/src/encryptdecrypt/Main.java
+++ Encryption-Decryption/task/src/encryptdecrypt/Main.java
@@ -1,8 +1,21 @@
 package encryptdecrypt;
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-
-        System.out.println("dv ulfmw z givzhfiv!");
+        Scanner scanner = new Scanner(System.in);
+        String message = scanner.nextLine();
+        int key = scanner.nextInt();
+        for(int i=0; i<message.length();i++) {
+            char temp = message.charAt(i);
+            if(temp>=97 && temp<=122){
+                temp += key;
+                if(temp > 122){
+                    temp = (char)(96 + (temp - 122));
+                }
+                System.out.print(temp);
+            } else if(message.charAt(i) == ' '){
+                System.out.print(" ");
+            }
+        }
     }
 }
