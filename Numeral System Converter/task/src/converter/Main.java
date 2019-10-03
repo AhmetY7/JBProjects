d361adb9c8958b91d37b983994a9f7412ffa677a
AhmetY7 <ahmetylmz95@gmail.com>
2019-10-03 23:59:40 +0300

    Stage #5 is continuing

--- Numeral System Converter/task/src/converter/Main.java
+++ Numeral System Converter/task/src/converter/Main.java
@@ -1,10 +1,10 @@
-package converter;
+package com.company;
 
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-    private static void convertToRadix(int radix, int number) {
+    private static String convertToRadix(int radix, int number) {
         ArrayList<Integer> result = new ArrayList<>();
         while (number >= radix) {
             result.add(number % radix);
@@ -19,7 +19,7 @@ public class Main {
                 convertedRadix += Integer.toString(result.get(i));
             }
         }
-        System.out.println(convertedRadix);
+        return convertedRadix;
     }
     private static int convertTo10(int sourceRadix, String number) {
         int total = 0;
@@ -28,19 +28,48 @@ public class Main {
         }
         return total;
     }
+    private static String convertFractionToRadix(int radix, double number) {
+        StringBuilder result = new StringBuilder();
+        for(int i=0; i<5; i++) {
+            if(i==Double.toString(number).length())
+                break;
+            StringBuilder temp = new StringBuilder(Double.toString(number * radix));
+            result.append(temp.charAt(0));
+            temp.replace(0,1,"0");
+            number = Double.parseDouble(temp.toString());
+        }
+        return result.toString();
+    }
+    private static double convertFractionTo10(int sourceRadix, String number) {
+        double result = 0.0;
+        for(int i=0; i<number.length(); i++) {
+            result += Integer.parseInt(number.charAt(i)+ "") * (1/Math.pow(sourceRadix,i+1));
+        }
+        return result;
+    }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int sourceRadix = Integer.parseInt(scanner.nextLine());
         String number = scanner.nextLine();
+        String[] fractions = number.split("\\.");
         int radix = Integer.parseInt(scanner.nextLine());
         if(sourceRadix != 10) {
-            convertToRadix(radix,convertTo10(sourceRadix, number));
+            int intTo10 = convertTo10(sourceRadix, fractions[0]);
+            double fractionTo10 = convertFractionTo10(sourceRadix, fractions[1]);
+            String intPart = convertToRadix(radix, intTo10);
+            String fractionPart = convertFractionToRadix(radix, fractionTo10);
+            System.out.print(intPart + "." + fractionPart);
         } else if (radix == 1) {
             for(int i=0; i<Integer.parseInt(number); i++) {
                 System.out.print("1");
             }
         } else {
-            convertToRadix(radix,Integer.parseInt(number));
+            String intPart = convertToRadix(radix,Integer.parseInt(fractions[0]));
+            String fractionPart = convertFractionToRadix(radix, Double.parseDouble("0." + fractions[1]));
+            System.out.print(intPart + "." + fractionPart);
         }
     }
 }
+
+
+
